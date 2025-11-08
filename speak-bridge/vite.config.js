e3ac42401f4c6eb4c240107efd3decf538e516b3
Elvis Ortiz <ortizjonathan144@gmail.com>
2025-11-08 08:08:58 -0500

    chore: add vite.config.js for project setup

--- speak-bridge/vite.config.js
+++ speak-bridge/vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' //allows tailwind
 
 // https://vite.dev/config/
-
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
-      proxy: {
-        "/api": { target: "http://localhost:8000", changeOrigin: true },
-      },
-    },
+    open: true, // keeps auto-open behavior
+  },
 })
